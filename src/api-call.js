bf6a89d3c52b6caebb487c8ceb4a9d308abd7f5f
Andrew Beach <38469540+epicsledge0510@users.noreply.github.com>
2023-03-06 04:52:31 -0500

    Update api-call.js

--- src/api-call.js
+++ src/api-call.js
@@ -1,9 +1,19 @@
 //This method loads the exercises from the api to be processed by the server
 require('dotenv').config()
 const getPost = () => {
-    fetch(process.env.MONGODB_URI)
-        .then(response => response.json())
-        .then(data => console.log(data))
+    fetch(process.env.MONGODB_URI, {
+    method: 'GET',
+    credentials: 'same-origin',
+    redirect: 'follow',
+    agent: null,
+    headers: {
+        "Content-Type": "text/plain",
+        'Authorization': 'Basic ' + btoa('username:password'),
+    },
+    timeout: 5000
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
 };
 //This method takes in a name and exercise and posts it to the api as a submission
 const makePost = async (name, exercise) => {
